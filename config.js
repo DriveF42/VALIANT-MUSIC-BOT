1c9b13bb1d08fa4a4f7a7fe2fdd3c4358ac603e0
DriveF42 <loveyujinfasfasf@gmail.com>
2024-10-23 06:44:44 +1200

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["1298354420240875523", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "nodev4.fypmoon.org",
-        port: 1118,
-        password: "fypmoon.org",
+        host: "node.lewdhutao.my.eu.org",
+        port: 80,
+        password: "youshallnotpass",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
